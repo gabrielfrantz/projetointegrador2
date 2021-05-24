54a183c6a65f6b1cc229ed4b10237ed0fe97d1f4
Rafael <saargov97@gmail.com>
2021-05-24 08:20:32 -0300

    Script validate token

---
+++ server/src/middleware.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+
+function checkToken(req, res, next) {
+    let token = req.headers['x-access-token'] || req.headers['authorization'];
+
+    if (token) {
+        if (token.startsWith('Bearer ')) {
+            token = token.slice(7, token.length);
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET || 'secret', (err, user) => {
+            if (err) {
+                res.status(401).json({
+                    success: false,
+                    message: 'Token is not valid!'
+                });
+            } else {
+                req.user = user;
+                next();
+            }
+        });
+    } else {
+        res.status(500).json({
+            success: false,
+            message: 'Auth token is not supplied!'
+        });
+    }
+}
+
+export default { checkToken }
